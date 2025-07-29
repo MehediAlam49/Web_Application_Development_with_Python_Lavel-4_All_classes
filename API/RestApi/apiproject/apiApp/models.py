8304b5ca868b6044e8439f9db52b21f17c21effd
MehediAlam49 <mehedialam806@gmail.com>
2025-07-29 13:02:15 +0600

    updated

--- API/RestApi/apiproject/apiApp/models.py
+++ API/RestApi/apiproject/apiApp/models.py
@@ -5,6 +5,8 @@ from django.db import models
 class StudentModel(models.Model):
     student_name = models.CharField(max_length=100,null=True)
     age = models.IntegerField(null=True)
+    roll = models.PositiveBigIntegerField(null=True)
+    username = models.CharField(max_length=100, null=True)
     address = models.CharField(max_length=100,null=True)
     register_date = models.DateTimeField(auto_now_add=True,null=True)
     def __str__(self):
--- API/RestApi/apiproject/apiApp/serializers.py
+++ API/RestApi/apiproject/apiApp/serializers.py
@@ -6,3 +6,8 @@ class studentSerializer(serializers.ModelSerializer):
     class Meta:
         model = StudentModel
         fields = '__all__'
+
+    def create(self, validated_data):
+        latest_roll = StudentModel.objects.order_by(-roll)
+        student_data = StudentModel.objects.create(**validated_data)
+        student_data.roll
