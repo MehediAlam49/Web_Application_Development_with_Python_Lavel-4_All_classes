ab0ed01d4e1017d9470871d0404a2ab08e4ec891
MehediAlam49 <mehedialam806@gmail.com>
2025-08-09 13:11:08 +0600

    add signup login and logout

--- Django_Classes/basic_school_project/basic_school_project/schoolApp/models.py
+++ Django_Classes/basic_school_project/basic_school_project/schoolApp/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class CustomUserModel(AbstractUser):
+    phone = models.CharField(max_length=20, null=True, blank=True)
+    USER_TYPE = [
+        ('Admin', 'Admin'),
+        ('Teacher', 'Teacher'),
+        ('Student', 'Student'),
+    ]
+    user_type = models.CharField(max_length=20, choices=USER_TYPE,default='Admin')
+
+    def __str__(self):
+        return self.username
+    
+class TeacherModel(models.Model):
+    user = models.OneToOneField(CustomUserModel, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100,null=True)
+    designation = models.TextField(null=True)
+    address = models.CharField(max_length=100,null=True)
+    def __str__(self):
+        return self.name
+    
+class TeacherModel(models.Model):
+    user = models.OneToOneField(CustomUserModel, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100,null=True)
+    class_name = models.CharField(max_length=100,null=True)
+    address = models.CharField(max_length=100,null=True)
+    def __str__(self):
+        return self.name
+    
--- Django_Classes/basic_school_project/basic_school_project/schoolApp/views.py
+++ Django_Classes/basic_school_project/basic_school_project/schoolApp/views.py
@@ -1,3 +1,48 @@
-from django.shortcuts import render
+
+from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate,login,logout,update_session_auth_hash
+from django.contrib.auth.decorators import login_required
+from schoolApp.models import *
 
 # Create your views here.
+def signupPage(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        phone = request.POST.get('phone')
+        password = request.POST.get('password')
+        user_type = request.POST.get('uer_type')
+        confirm_password = request.POST.get('confirm_password')
+        
+        if password == confirm_password:
+            CustomUserModel.objects.create_user(
+                username=username,
+                email=email,
+                phone=phone,
+                user_type=user_type,
+                password=confirm_password,
+            )
+            return redirect('loginPage')
+    return render(request, 'signupPage.html')
+
+
+def loginPage(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        
+        user = authenticate(username=username,password=password)
+        if user:
+            login(request, user)
+            return redirect('home')
+    return render(request, 'loginPage.html')
+
+def logoutPage(request):
+    logout(request)
+    return redirect('loginPage')
+
+def home(request):
+    return render(request, 'home.html')
+
+def addTeacher(request):
+    return render(request, 'addTeacher.html')
