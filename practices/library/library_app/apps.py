6a6db5c2cb3b27d7c8bca5c78858462b1b83a2f8
MehediAlam49 <mehedialam806@gmail.com>
2025-08-20 09:07:55 +0600

    create library authentictaion and form project

--- practices/library/library_app/apps.py
+++ practices/library/library_app/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class UserAuthConfig(AppConfig):
+class LibraryAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'user_auth'
+    name = 'library_app'
